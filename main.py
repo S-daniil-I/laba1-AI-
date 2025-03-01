fc117e25d6d86a339465a354eac2bc4df74698de
Seledtsov Daniil <danikseledtsov@gmail.com>
2025-03-02 00:06:24 +0300

    Changed dataset

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder,MinMaxScaler, StandardScaler
 
-train_data=pd.read_csv(r'D:\РАФ\2 курс\4сем\AI\data titan\train.csv')
+train_data=pd.read_csv(r'D:\РАФ\2 курс\4сем\AI\data titan(example)\train.csv')
 missing_values = train_data.isnull().sum()
-train_data['Title'] = train_data['Name'].str.extract(r' ([A-Za-z]+)\.', expand=False)
 
-categorical_cols = [ 'Sex', 'Cabin', 'Embarked', 'Title']
-num_columns = ['Age', 'SibSp', 'Parch', 'Fare']
+categorical_cols = ['HomePlanet', 'CryoSleep', 'Cabin', 'Destination', 'VIP','Name']
+num_columns = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
 
 for column in categorical_cols:
     mode_value = train_data[column].mode()[0]
@@ -21,7 +20,6 @@ print(f'Missing values after fillings:{after_missing_values}')
 
 scaler = StandardScaler()
 train_data[num_columns] = scaler.fit_transform(train_data[num_columns])
-train_data.to_csv("processed_titanic",index=False)
 
 train_data = pd.get_dummies(train_data, columns=categorical_cols, drop_first=True)
 train_data.to_csv("processed_titanic.csv",index=False)
