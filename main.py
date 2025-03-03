a36829fee50ddf072889af13188c8dbce0b5e3bc
Seledtsov Daniil <danikseledtsov@gmail.com>
2025-03-03 17:57:36 +0300

    broken into small peaces variable cabin

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder,MinMaxScaler, StandardScaler
 
 train_data=pd.read_csv(r'D:\РАФ\2 курс\4сем\AI\data titan(example)\train.csv')
+train_data[['Deck', 'Cabin_num', 'Side']] = train_data["Cabin"].str.split("/", expand=True)
+train_data['Cabin_num'] = train_data['Cabin_num'].astype(float)
+
 train_data = train_data.drop(columns=['Cabin', 'Name'])
 missing_values = train_data.isnull().sum()
 
-categorical_cols = ['HomePlanet','CryoSleep','Destination','VIP']
-num_columns = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck']
+categorical_cols = ['HomePlanet','CryoSleep','Destination','VIP','Deck','Side']
+num_columns = ['Age', 'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck','Cabin_num']
 
 for column in categorical_cols:
     mode_value = train_data[column].mode()[0]
@@ -29,7 +32,7 @@ train_data['CryoSleep'] = train_data['CryoSleep'].astype(int)
 scaler = StandardScaler()
 train_data[num_columns] = scaler.fit_transform(train_data[num_columns])
 
-train_data = pd.get_dummies(train_data, columns=categorical_cols_no_bin, drop_first=True)
+train_data = pd.get_dummies(train_data, columns=categorical_cols_no_bin, drop_first=False)
 train_data = train_data.replace({True: 1, False: 0})
 
 train_data.to_csv("processed_titanic.csv",index=False)
